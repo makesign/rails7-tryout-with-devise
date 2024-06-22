a3819b133fde3f9fc9ef28f7ad8e25c1159d552a
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-06-22 13:07:36 +0200

    add mail config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
 require_relative "application"
 
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  port: ENV['SMTP_PORT'],
+  address: ENV['SMTP_SERVER'],
+  user_name: ENV['SMTP_LOGIN'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
