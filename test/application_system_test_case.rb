5200194a9b073ce000834443200e327b7146e158
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-09-03 13:02:04 +0200

    use headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |driver_option|
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_option|
       driver_option.add_argument('--disable-search-engine-choice-screen')
   end
 end
