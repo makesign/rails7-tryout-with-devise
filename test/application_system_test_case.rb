23728fa81ed840a9de800be8a3f57ae789195446
Airon <aironsamuel@googlemail.com>
2024-09-05 15:54:49 +0200

    Chrome selenium fix

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-end
+
+  :chrome :headless_chrome 
+  # :firefox :headless_firefox
+     
+  config = SystemTestConfig.new :headless_firefox
+
+  driven_by :selenium, using: config.driver, 
+    screen_size: [1400, 1400],
+    options: config.driver_options do |driver_option|
+        driver_option.add_argument('--disable-search-engine-choice-screen')
+    end
